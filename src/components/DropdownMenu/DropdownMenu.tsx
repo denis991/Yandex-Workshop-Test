c5cb5871b2316cfd7e953b31f54de262eb958e98
Denis Gutsulak <d.99113@gmail.com>
2023-06-26 12:42:58 +0300

    v2

--- src/components/DropdownMenu/DropdownMenu.tsx
+++ src/components/DropdownMenu/DropdownMenu.tsx
@@ -1,98 +1,112 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, {
+	useState,
+	useEffect,
+	useRef,
+	// ReactNode,
+	ReactElement,
+	// JSXElementConstructor,
+} from 'react';
 import './DropdownMenu.css';
 
 type DropdownMenuProps = {
-  trigger: React.ReactNode;
-  content: React.ReactNode;
-  onItemSelect?: (item: string) => void;
+	trigger: ReactElement<{ onClick: () => void }>;
+	content: ReactElement<{ children: string }> | ReactElement<{ children: string }>[] | null;
+	// onItemSelect?: (item: string) => void;
 };
 
 type Position = 'bottom-right' | 'top-right' | 'bottom-left' | 'top-left';
 
-const DropdownMenu: React.FC<DropdownMenuProps> = ({
-  trigger,
-  content,
-  onItemSelect
-}) => {
-  const [isOpen, setIsOpen] = useState(false);
-  const [position, setPosition] = useState<Position>('bottom-right');
-  const dropdownRef = useRef<HTMLDivElement>(null);
+const DropdownMenu: React.FC<DropdownMenuProps> = ({ trigger, content, onItemSelect }) => {
+	const [isOpen, setIsOpen] = useState(false);
+	const [position, setPosition] = useState<Position>('bottom-right');
+	const dropdownRef = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
-    function handleClickOutside(event: MouseEvent) {
-      if (
-        dropdownRef.current &&
-        !dropdownRef.current.contains(event.target as Node)
-      ) {
-        setIsOpen(false);
-      }
-    }
+	useEffect(() => {
+		function handleClickOutside(event: MouseEvent) {
+			if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+				setIsOpen(false);
+			}
+		}
 
-    document.addEventListener('mousedown', handleClickOutside);
-    return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
-    };
-  }, []);
+		document.addEventListener('mousedown', handleClickOutside);
+		return () => {
+			document.removeEventListener('mousedown', handleClickOutside);
+		};
+	}, []);
 
-  const handleTriggerClick = () => {
-    setIsOpen(!isOpen);
-  };
+	// const handleTriggerClick = () => {
+	// 	setIsOpen(!isOpen);
+	// };
 
-  const calculatePosition = () => {
-    if (!dropdownRef.current) return;
+	const calculatePosition = () => {
+		if (!dropdownRef.current) return;
 
-    const { top, bottom, left, right } = dropdownRef.current.getBoundingClientRect();
-    const { innerWidth, innerHeight } = window;
+		const { top, bottom, left, right } = dropdownRef.current.getBoundingClientRect();
+		const { innerWidth, innerHeight } = window;
 
-    const hasEnoughSpaceBottom = bottom + 260 <= innerHeight;
-    const hasEnoughSpaceTop = top - 260 >= 0;
-    const hasEnoughSpaceRight = right + 260 <= innerWidth;
-    const hasEnoughSpaceLeft = left - 260 >= 0;
+		const hasEnoughSpaceBottom = bottom + 260 <= innerHeight;
+		const hasEnoughSpaceTop = top - 260 >= 0;
+		const hasEnoughSpaceRight = right + 260 <= innerWidth;
+		const hasEnoughSpaceLeft = left - 260 >= 0;
 
-    if (hasEnoughSpaceBottom) {
-      setPosition('bottom-right');
-    } else if (hasEnoughSpaceTop) {
-      setPosition('top-right');
-    } else if (hasEnoughSpaceLeft) {
-      setPosition('bottom-left');
-    } else if (hasEnoughSpaceRight) {
-      setPosition('top-left');
-    }
-  };
+		if (hasEnoughSpaceBottom) {
+			setPosition('bottom-right');
+		} else if (hasEnoughSpaceTop) {
+			setPosition('top-right');
+		} else if (hasEnoughSpaceLeft) {
+			setPosition('bottom-left');
+		} else if (hasEnoughSpaceRight) {
+			setPosition('top-left');
+		}
+	};
 
-  useEffect(() => {
-    if (isOpen) {
-      calculatePosition();
-    }
-  }, [isOpen]);
+	useEffect(() => {
+		if (isOpen) {
+			calculatePosition();
+		}
+	}, [isOpen]);
 
-  const handleItemClick = (item: string) => {
-    if (onItemSelect) {
-      onItemSelect(item);
-    }
-    setIsOpen(false);
-  };
+	// const handleItemClick = (item: string) => {
+	// 	if (onItemSelect) {
+	// 		onItemSelect(item);
+	// 	}
+	// 	setIsOpen(false);
+	// };
 
-  return (
-    <div className="dropdown" ref={dropdownRef}>
-      <div className="trigger" onClick={handleTriggerClick}>
-        {trigger}
-      </div>
-      {isOpen && (
-        <div className={`content ${position}`}>
-          {React.Children.map(content, (child, index) => (
-            <div
-              key={index}
-              className="item"
-              onClick={() => handleItemClick(child.props.children as string)}
-            >
-              {child}
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
-  );
+	const handleItemClick = (child: string) => {
+		console.log(`Clicked on item: ${child}`);
+		setIsOpen(false);
+	};
+	const toggleMenu = () => {
+		setIsOpen((prevState) => !prevState);
+	};
+
+	return (
+		<div className='dropdown-menu' ref={dropdownRef}>
+			{/* <div className='trigger' onClick={handleTriggerClick}> */}
+			<div className='trigger' onClick={toggleMenu}>
+				{trigger}
+			</div>
+
+			{isOpen && content && (
+				<div className={`content ${position}`}>
+					{React.Children.map(content, (child: React.ReactElement<{ children: string }>, index) => {
+						if (child) {
+							return (
+								<div
+									key={index}
+									className='item'
+									onClick={() => handleItemClick(child.props.children as string)}>
+									{child}
+								</div>
+							);
+						}
+						return null;
+					})}
+				</div>
+			)}
+		</div>
+	);
 };
 
 export default DropdownMenu;
