da3068d2762a57362088c0916f94606cb7b1e7d7
Denis Gutsulak <d.99113@gmail.com>
2023-06-26 18:39:30 +0300

    v6

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,59 @@
 import React from 'react';
 import DropdownMenu from './components/DropdownMenu/DropdownMenu';
 import { Share2, Edit, Trash } from 'react-feather'; // Подключение иконок из feather-icons
+import './App.css';
+
+interface DropdownDirection {
+	direction: string[];
+}
 
 const App: React.FC = () => {
-  const handleShareClick = () => {
-    console.log('Share item clicked');
-  };
+	const handleShareClick = () => {
+		console.log('Share item clicked');
+	};
 
-  const handleEditClick = () => {
-    console.log('Edit item clicked');
-  };
+	const handleEditClick = () => {
+		console.log('Edit item clicked');
+	};
 
-  const handleDeleteClick = () => {
-    console.log('Delete item clicked');
-  };
+	const handleDeleteClick = () => {
+		console.log('Delete item clicked');
+	};
 
-  return (
-    <div>
-      <DropdownMenu
-        trigger={<button>Open Dropdown</button>}
-        content={[
-          {
-            label: 'Поделиться в социальных сетях',
-            icon: <Share2 />,
-            onClick: handleShareClick
-          },
-          {
-            label: 'Редактировать страницу',
-            icon: <Edit />,
-            onClick: handleEditClick
-          },
-          {
-            label: 'Удалить страницу',
-            icon: <Trash />,
-            onClick: handleDeleteClick
-          }
-        ]}
-      />
-    </div>
-  );
+	const dropdownDirection: DropdownDirection = {
+		direction: ['up-left', 'up-right', 'down-left', 'down-right',  ],
+	};
+
+	return (
+		<div className='dropdown-container'>
+			{dropdownDirection.direction.map((item, index) => (
+				<div
+					key={`${index} Direction ${Date.now() + Math.random()}`}
+					className={` ${item}`}>
+					<DropdownMenu
+						trigger={<button>…</button>}
+						content={[
+							{
+								label: 'Поделиться в социальных сетях',
+								icon: <Share2 />,
+								onClick: handleShareClick,
+							},
+							{
+								label: 'Редактировать страницу',
+								icon: <Edit />,
+								onClick: handleEditClick,
+							},
+							{
+								label: 'Удалить страницу',
+								icon: <Trash />,
+								onClick: handleDeleteClick,
+							},
+						]}
+					/>
+				</div>
+			))}
+		</div>
+	);
 };
 
 export default App;
